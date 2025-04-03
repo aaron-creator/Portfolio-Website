e0491db931c10d5ca50e0f86acf57a9b8007f1fd
aaron_tetracube <aaronpsam@gmail.com>
2025-04-03 12:00:28 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { React } from "react";
+import { Home } from "./components/Home";
 function App() {
   return (
     <div className="container mt-5">
       <h1 className="text-primary">Hello Bootstrap in React!</h1>
       <button className="btn btn-success">Click Me</button>
+      <Home />
     </div>
   );
 }
